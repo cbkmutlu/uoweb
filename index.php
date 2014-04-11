99be5c6f87cb7892b0d58e39ba131f6fac5e3b99
Mark Sturgill <msturgill@gmail.com>
2014-04-11 16:36:40 -0700

    init

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+$ip = $_SERVER['REMOTE_ADDR'];
+if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER)) {
+  $ip = array_pop(explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']));
+}
+echo $ip;
+?>
