7b4735d5397078079ee897098f5f367c08224284
Mark Sturgill <msturgill@gmail.com>
2014-05-03 16:36:40 -0700

    hmm

--- api/v1/itemart.php
+++ api/v1/itemart.php
@@ -55,11 +55,11 @@ if (isset($hue)) {
 				$g = ($c >> 8) & 0xFF;
 				$b = ($c) & 0xFF;
 $scale = 31.0 / 255;
-$red = intval($r * $scale)&0x1F;
+$red = round($r * $scale)&0x1F;
 if ($red == 0 && $r != 0) $red = 1;
-$green = intval($g * $scale)&0x1F;
+$green = round($g * $scale)&0x1F;
 if ($green == 0 && $g != 0) $green = 1;
-$blue = intval($b * $scale)&0x1F;
+$blue = round($b * $scale)&0x1F;
 if ($blue == 0 && $b != 0) $blue = 1;
 
 				if ($red == $green && $red == $blue) {
