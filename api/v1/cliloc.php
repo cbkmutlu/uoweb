6ecafb10a0d4bd192b469964cafef0a76466fc0c
Mark Sturgill <msturgill@gmail.com>
2014-05-03 02:55:29 -0700

    api

--- api/v1/cliloc.php
+++ api/v1/cliloc.php
@@ -5,13 +5,16 @@ if (!array_key_exists('n', $_REQ))
 	return;
 
 $n = preg_replace('/\D/', '', $_REQ['n']);
+
 if (!$n)
 	return;
 
+$n = intval($n);
+
 include_once 'inc/mongo.php';
 
-$c = $md->cliloc;
-$data = $c->find([_id => $n],['_id' => false])['text'];
+$c = $md->cliloc_enu;
+$data = $c->find(['_id' => $n],['_id' => false])['text'];
 
 header('Vary: Accept-Encoding');
 echo $data;
