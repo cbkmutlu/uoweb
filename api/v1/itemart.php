5781e3f2e9559cdd87936addf960a6cb8e0eee1f
Mark Sturgill <msturgill@gmail.com>
2014-05-03 15:25:09 -0700

    hmm

--- api/v1/itemart.php
+++ api/v1/itemart.php
@@ -43,7 +43,7 @@ if (isset($hue)) {
 			for($j = 0; $j < $y; $j++) {
 				$c = imagecolorat($img, $i, $j);
 
-				if (!$c || $c == 0xFFFFFFFF)
+				if (!$c || $c == 0xFFFFFF)
 					continue;
 
 				$r = ($c >> 16) & 0x1F;
